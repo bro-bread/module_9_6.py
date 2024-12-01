73a957139521b4e495c30e5338794273001f08b0
bro-bread <sasaglebov928@gmail.com>
2024-12-01 07:30:34 +0700

    Update module_9_6.py

--- module_9_6.py
+++ module_9_6.py
@@ -1,15 +1,36 @@
 from itertools import *
 
 def all_variants(text):
-    variants = []
+    list_tr = []
+    variants = list(text)
     for i in range(len(text)):
-        variants.extend(product(text, repeat=i+1))
-    for t in variants:
+        list_tr.append(variants[i])
+
+
+    for r in range(len(text)):
+        for t in range(len(text)):
+            if r < t == 1:
+                rrr = str(variants[r] + variants[t])
+                list_tr.append(rrr)
+            if t > r == 1:
+                ttt = str(variants[t] + variants[r])
+                list_tr.append(ttt)
+
+    list_tr.append(text)
+
+    for t in list_tr:
         if print(*t) != None:
             yield print(*t)
 
 
 text = "abc"
 fff = all_variants(text)
-for variant in fff:
-    print(variant)
+
+for nnn in fff:
+    print(nnn)
+
+
+
+
+
+
